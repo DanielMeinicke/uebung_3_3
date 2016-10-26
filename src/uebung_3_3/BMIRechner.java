75d4e4961ff520b5812b48ea9d04d3aea7e49988
danie <danie@10.43.6.124>
2016-10-26 13:05:45 +0200

    BMIRechner korrektur

--- src/uebung_3_3/BMIRechner.java
+++ src/uebung_3_3/BMIRechner.java
@@ -5,10 +5,10 @@ public class BMIRechner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double koerperGroesse = 2.00, gewicht = 95.0;
+		double koerperGroesse = 1.60, gewicht = 70.0;
 		double bmi;
-		int alter = 21;
-		boolean männlich = true;
+		int alter = 33;
+		boolean männlich = false;
 
 		// Berechnung BMI
 		bmi = gewicht / Math.pow(koerperGroesse, 2);
@@ -22,7 +22,8 @@ public class BMIRechner {
 				}
 				if (bmi > 25) {
 					System.out.println("Sie sind übergewichtig!");
-				} else {
+				}
+				if (bmi <= 25 && bmi >= 20) {
 					System.out.println("Sie haben Normalgewicht!");
 				}
 			} else {
@@ -31,123 +32,134 @@ public class BMIRechner {
 				}
 				if (bmi > 24) {
 					System.out.println("Sie sind übergewichtig!");
-				} else {
+				}
+				if (bmi <= 24 && bmi >= 19) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
+			}
+
+		}
+		if (alter >= 25 && alter <= 34) {
+			if (männlich == true) {
+				if (bmi < 21) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 26) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 26 && bmi >= 21) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
+			} else {
+				if (bmi < 20) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 25) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 25 && bmi >= 20) {
 					System.out.println("Sie haben Normalgewicht!");
 				}
 
 			}
-			if (alter > 24) {
-				if (männlich == true) {
-					if (bmi < 21) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 26) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-				} else {
-					if (bmi < 20) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 25) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
 
+		}
+		if (alter >= 35 && alter <= 44) {
+			if (männlich == true) {
+				if (bmi < 22) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 27) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 27 && bmi >= 22) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
+			} else {
+				if (bmi < 21) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 26) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 26 && bmi >= 21) {
+					System.out.println("Sie haben Normalgewicht!");
 				}
 
 			}
-			if (alter > 34) {
-				if (männlich == true) {
-					if (bmi < 22) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 27) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-				} else {
-					if (bmi < 21) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 26) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
 
+		}
+		if (alter >= 45 && alter <= 54) {
+			if (männlich == true) {
+				if (bmi < 23) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 28) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 28 && bmi >= 23) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
+			} else {
+				if (bmi < 22) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 27) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 27 && bmi >= 22) {
+					System.out.println("Sie haben Normalgewicht!");
 				}
 
 			}
-			if (alter > 44) {
-				if (männlich == true) {
-					if (bmi < 23) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 28) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-				} else {
-					if (bmi < 22) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 27) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-
+		}
+		if (alter >= 55 && alter <= 64) {
+			if (männlich == true) {
+				if (bmi < 24) {
+					System.out.println("Sie sind untergewichtig!");
 				}
-			}
-			if (alter >= 64) {
-				if (männlich == true) {
-					if (bmi < 24) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 29) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-				} else {
-					if (bmi < 23) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 28) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-
+				if (bmi > 29) {
+					System.out.println("Sie sind übergewichtig!");
 				}
-			}
-			if (alter > 64) {
-				if (männlich == true) {
-					if (bmi < 25) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 30) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
-				} else {
-					if (bmi < 24) {
-						System.out.println("Sie sind untergewichtig!");
-					}
-					if (bmi > 29) {
-						System.out.println("Sie sind übergewichtig!");
-					} else {
-						System.out.println("Sie haben Normalgewicht!");
-					}
+				if (bmi <= 29 && bmi >= 24) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
+			} else {
+				if (bmi < 23) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 28) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 38 && bmi >= 23) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
 
+			}
+		}
+		if (alter > 64) {
+			if (männlich == true) {
+				if (bmi < 25) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 30) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 30 && bmi >= 25) {
+					System.out.println("Sie haben Normalgewicht!");
+				}
+			} else {
+				if (bmi < 24) {
+					System.out.println("Sie sind untergewichtig!");
+				}
+				if (bmi > 29) {
+					System.out.println("Sie sind übergewichtig!");
+				}
+				if (bmi <= 29 && bmi >= 24) {
+					System.out.println("Sie haben Normalgewicht!");
 				}
 			}
 		}
 	}
+
 }
